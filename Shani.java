82b53acff5bee96c774c59164024b59e1c9dd23c
ShaniLamy <shaninchuk@gmail.com>
2024-02-01 15:16:56 +0200

    Shani.java was added

--- Shani.java
+++ Shani.java
@@ -1,7 +1,7 @@
 public class Shani{
 	public static void main(String[] args){
 			for(int i=1;i<=20;i++){
-				System.out.println("Shani "+i);
+				System.out.println("Hello Shani "+i);
 			}
 	}
 }
